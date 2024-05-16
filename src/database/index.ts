8227ea4d90bd19cb8005c00d11509f6c1f73c970
pvillaverde <31769405+pvillaverde@users.noreply.github.com>
2024-05-16 22:30:37 +0200

    🐛 Fix database connection issue

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,17 @@
 import mysql from "npm:mysql2";
 import dbConfig from "../config/db.config.ts";
+import logger from "../services/logger.service.ts";
 
-export default mysql.createConnection({
+const connection = mysql.createConnection({
    host: dbConfig.DB_HOST,
    user: dbConfig.DB_USER,
    password: dbConfig.DB_PASS,
    database: dbConfig.DB_NAME,
 });
+connection.connect(err => {
+   if (err) {
+      logger.fatal(err)
+      Deno.exit(1);
+   }
+})
+export default connection
