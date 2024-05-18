fc9b95c7af340e157bf7f095d73fcaea991076ed
pvillaverde <31769405+pvillaverde@users.noreply.github.com>
2024-05-18 19:09:54 +0200

    🚑 Fix database queries

--- src/database/repositories/twitchStream.repository.ts
+++ src/database/repositories/twitchStream.repository.ts
@@ -18,10 +18,10 @@ class TwitchStreamRepository implements ITwitchStreamStreamRepository {
       return new Promise((resolve, reject) => {
          connection.query(
             `INSERT INTO twitch_streams 
-            (id, type, user_id, user_login, user_name, game_id, game_name, title, viewer_count, started_at, ended_at, thumbnail_url, language, tags, is_mature, live_messages)
-            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-            [stream.id, stream.type, stream.user_id, stream.user_login, stream.game_id, stream.game_name, stream.title, stream.viewer_count, new Date(stream.started_at), new Date(),
-            stream.thumbnail_url, stream.language, stream.tags ? stream.tags.join(', ') : null, stream.is_mature, stream.live_messages],
+            (id, type, user_id, user_login, user_name, game_id, game_name, title, viewer_count, started_at, ended_at, thumbnail_url, language, tags, is_mature)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+            [stream.id, stream.type, stream.user_id, stream.user_login, stream.user_name, stream.game_id, stream.game_name, stream.title, stream.viewer_count, new Date(stream.started_at), new Date(),
+            stream.thumbnail_url, stream.language, stream.tags ? stream.tags.join(', ') : null, stream.is_mature],
             (err: any, _res: ITwitchStream | PromiseLike<ITwitchStream>) => {
                if (err && err.code == "ER_DUP_ENTRY") {
                   this.update(stream)
@@ -44,15 +44,15 @@ class TwitchStreamRepository implements ITwitchStreamStreamRepository {
          this.retrieveById(stream.id).then(oldStream => {
             connection.query(
                `UPDATE twitch_streams SET
-               type=?, user_id=?, user_login=?, user_name=?, game_id=?, game_name=?, title=?, viewer_count=?, started_at=?, ended_at=?, thumbnail_url=?, language=?, tags=?, is_mature=?, live_messages=?
+               type=?, user_id=?, user_login=?, user_name=?, game_id=?, game_name=?, title=?, viewer_count=?, started_at=?, ended_at=?, thumbnail_url=?, language=?, tags=?, is_mature=?
                WHERE id=?`,
-               [stream.type, stream.user_id, stream.user_login,
+               [stream.type, stream.user_id, stream.user_login, stream.user_name,
                // Prevent null values when changed stream type or no longer active
                stream.game_id && stream.game_id.length > 0 ? stream.game_id : oldStream.game_id,
                stream.game_name && stream.game_name.length > 0 ? stream.game_name : oldStream.game_name,
                // --
                stream.title, stream.viewer_count, new Date(stream.started_at), new Date(),
-               stream.thumbnail_url, stream.language, stream.tags && Array.isArray(stream.tags) ? stream.tags.join(', ') : stream.tags, stream.is_mature, stream.live_messages, stream.id],
+               stream.thumbnail_url, stream.language, stream.tags && Array.isArray(stream.tags) ? stream.tags.join(', ') : stream.tags, stream.is_mature, stream.id],
                (err: any, _res: ITwitchStream | PromiseLike<ITwitchStream>) => {
                   if (err) {
                      reject(err);
