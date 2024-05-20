dd7bc8c7fcb1bca487eb50c565fbf0585c93615b
pvillaverde <pvillaverde.git@gmail.com>
2024-05-20 19:57:08 +0200

    ✨ Allow LogLevel customization through env var

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,6 +1,24 @@
 import { Logger } from "npm:tslog";
 import LogLevel from "../types/logLevel.ts";
+
+// Create a mapping of log level strings to LogLevel enum values
+const logLevelMap: { [key: string]: LogLevel } = {
+   silly: LogLevel.Silly,
+   trace: LogLevel.Trace,
+   debug: LogLevel.Debug,
+   info: LogLevel.Info,
+   warn: LogLevel.Warn,
+   error: LogLevel.Error,
+   fatal: LogLevel.Fatal,
+};
+
+// Function to get the log level from the environment variable
+function getLogLevelFromEnv(): LogLevel {
+   const logLevelString = Deno.env.get("LOG_LEVEL")?.toLowerCase() || "info";
+   return logLevelMap[logLevelString] ?? LogLevel.Info;
+}
+
 export default new Logger({
-   minLevel: LogLevel.Info,
+   minLevel: getLogLevelFromEnv(),
    type: "pretty",
 });
